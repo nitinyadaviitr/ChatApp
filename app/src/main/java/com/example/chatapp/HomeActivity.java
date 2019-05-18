de31cec3ee267900f5a71643fa616037b0b41dfe
Nitin Yadav <nyadav@ee.iitr.ac.in>
2019-05-19 04:00:48 +0530

    Homepage UI
    UI for Homepage

--- app/src/main/java/com/example/chatapp/HomeActivity.java
+++ app/src/main/java/com/example/chatapp/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.chatapp;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        if(haveNetwork()){
+            //MainFunction
+        }
+        else if(!haveNetwork()){
+            Toast.makeText( HomeActivity.this, "Please connect to Internet", Toast.LENGTH_LONG).show();
+        }
+    }
+    private boolean haveNetwork()
+    {
+        boolean have_wifi = false;
+        boolean have_data = false;
+
+        ConnectivityManager connectivityManager=(ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+
+        for(NetworkInfo info:networkInfos)
+        {
+            if(info.getTypeName().equalsIgnoreCase( "WIFI"))
+            if(info.isConnected())
+                have_wifi = true;
+            if(info.getTypeName().equalsIgnoreCase( "MOBILE"))
+            if(info.isConnected())
+                have_data = true;
+        }
+        return have_data||have_wifi;
     }
 }
--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.chatapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIMEOUT = 3000;
 
+    TextView textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        textview = (TextView) findViewById(R.id.my_text);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "Fonts/AliandoRocky.ttf");
+        textview.setTypeface(myCustomFont);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
